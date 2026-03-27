a4f790c51c947efe2d5c3ec8dacef7384e6e0197
Your Name <you@example.com>
2026-03-26 23:07:53 -0400

    first commitasd

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,41 +22,5 @@ class DatabaseSeeder extends Seeder
                 'password' => 'password',
             ],
         );
-
-        $riders = [
-            ['rider_id' => 'SC00065', 'name' => 'SANDRA PARADA CABALLERO'],
-            ['rider_id' => 'SC00081', 'name' => 'JORGE MAMANI FLORES'],
-            ['rider_id' => 'SC00102', 'name' => 'MARIA QUISPE ROJAS'],
-        ];
-
-        foreach ($riders as $index => $data) {
-            $rider = \App\Models\Rider::query()->updateOrCreate(
-                ['rider_id' => $data['rider_id']],
-                ['name' => $data['name']],
-            );
-
-            if ($rider->movements()->exists()) {
-                continue;
-            }
-
-            $rider->movements()->createMany([
-                [
-                    'movement_type' => 'purchase',
-                    'reference' => 'FAC-' . (4100 + $index),
-                    'description' => 'Compra de lubricantes',
-                    'amount' => 350.00 + ($index * 25),
-                    'points' => 35 + ($index * 5),
-                    'occurred_at' => now()->subDays(10 - $index),
-                ],
-                [
-                    'movement_type' => 'purchase',
-                    'reference' => 'FAC-' . (4200 + $index),
-                    'description' => 'Compra de repuestos',
-                    'amount' => 180.00 + ($index * 10),
-                    'points' => 18 + ($index * 3),
-                    'occurred_at' => now()->subDays(3 - $index),
-                ],
-            ]);
-        }
     }
 }
